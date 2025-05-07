97e66d0faadfb97fc7d94971b25f7c2e24eccf69
funa <orz.home35@gmail.com>
2025-05-07 13:14:44 +0900

    📝 いらないファイルを系s他

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/08 01:21:55 by miyuu             #+#    #+#              #
-#    Updated: 2025/05/07 13:09:27 by miyuu            ###   ########.fr        #
+#    Updated: 2025/05/07 13:13:53 by miyuu            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,8 +19,7 @@ HEADER_DIR = include
 HEADER = $(HEADER_DIR)/cub3d.h
 
 # ここに追加していく
-SRC_FILES = main.c \
-			# main_minilibx.c
+SRC_FILES = main.c
 
 # ---------- Libft & GNL ---------- #
 
--- src/main.c
+++ src/main.c
@@ -3,23 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mfunakos <mfunakos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:44:56 by mfunakos          #+#    #+#             */
-/*   Updated: 2025/05/07 13:08:17 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/30 20:33:16 by mfunakos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
+
 int	main(int argc, char *argv[])
 {
-	(void) argv;
+	int		fd;
+	char	*line;
+	(void)argc;
+
 	printf("コンパイルできた〜〜〜\n");
-	if (argc != 2)
+
+	fd = open(argv[1], O_RDONLY);
+	line = get_next_line(fd);
+	while (line != NULL)
 	{
-		write(STDERR_FILENO, "Invalid number of arguments.\n", ft_strlen("Invalid number of arguments.\n"));
-		return (1);
+		printf("%s", line);
+		line = get_next_line(fd);
 	}
+	close(fd);
 	return (0);
 }
