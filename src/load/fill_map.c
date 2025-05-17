9ac465b1dfcde1bcbd973147d05decf437ab49da
funa <orz.home35@gmail.com>
2025-05-17 20:24:42 +0900

    🚧 mapの情報をenumで表すやつ作りy通

--- src/load/fill_map.c
+++ src/load/fill_map.c
@@ -6,11 +6,68 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 20:56:02 by miyuu             #+#    #+#             */
-/*   Updated: 2025/05/14 15:53:56 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/05/15 20:45:27 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+t_map_type	get_player_dir_enum(char str)
+{
+	if (str == '0' || str == 'N' || str == 'S' || str == 'E' || str == 'W')
+		return (EMPTY);
+	if (str == '1')
+		return (WALL);
+	if (str == ' ')
+		return (NOTHING);
+	//todo; error_print_and_exit("Invalid map value.");
+	return (-1);
+}
+
+unsigned int	get_longest_width(const t_strlst *lines)
+{
+	unsigned int	max;
+	unsigned int	now;
+
+	max = 0;
+	now = 0;
+	while (lines)
+	{
+		now = ft_strlen(lines->str);
+		if (now > max)
+			max = now;
+		lines = lines->next;
+	}
+	return (max);
+}
+
+void	fill_map_test(t_data *data, const t_strlst *lines)
+{
+	size_t		i;
+	size_t		j;
+	size_t		map_height;
+
+	//もし、空行だったらexitする todo:正規化の段階で空だったらエラー吐くようにする
+	data->height = str_lstsize(lines);
+	data->width = get_longest_width(lines);
+	data->map = (int *)ft_calloc(data->height * data->width + 1, sizeof(int));
+	if (!data->map)
+		error_perror_and_exit(NULL);
+	i = 0;
+	while (lines)
+	{
+		j = 0;
+		while (j < data->width)
+		{
+			if (lines->str[j])
+				data->map[i] = get_player_dir_enum(lines->str[j]);
+			else
+				data->map[i] = NOTHING;
+			i++;
+		}
+		lines = lines->next;
+	}
+}
 
 // cahr cが入って、対応するenumを返す関数を作る→もし、enumに入らない文字だったらどうするか考える。
 void	fill_map(t_data *d, char **map_lines)
--- src/load/fill_player_position.c
+++ src/load/fill_player_position.c
@@ -6,11 +6,24 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 21:01:28 by miyuu             #+#    #+#             */
-/*   Updated: 2025/05/12 00:00:58 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/05/15 20:26:52 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+t_player_dir	get_player_dir_enum(char dir)
+{
+	if ('N' == dir)
+		return (DIR_NORTH);
+	if ('S' == dir)
+		return (DIR_SOUTH);
+	if ('E' == dir)
+		return (DIR_EAST);
+	if ('W' == dir)
+		return (DIR_WEST);
+	return (-1);
+}
 
 void	fill_player_position(t_data *data)
 {
@@ -28,7 +41,7 @@ void	fill_player_position(t_data *data)
 				|| data->map[y][x] == 'E' \
 				|| data->map[y][x] == 'W')
 			{
-				data->player = (t_pos){y, x, data->map[y][x]};
+				data->player = (t_pos){y, x, get_player_dir_enum(data->map[y][x])};
 				//playerが正しく存在する前提なので。プレイヤー見つかったら終わる。ちゃんといるかをここで再度判定するのもあり
 				return ;
 			}
