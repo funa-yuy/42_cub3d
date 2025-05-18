04fbd2f70766e5c529c2ce8e39597aa27ae1884b
funa <orz.home35@gmail.com>
2025-05-18 13:59:48 +0900

    🛠️ init_test00を作った.

---
+++ test/unit-tests/load/check_t_data_structure.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_t_data_structure.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 13:13:19 by miyuu             #+#    #+#             */
+/*   Updated: 2025/05/18 13:52:48 by miyuu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+/* t_data構造体の*/
+/* 構造体の各フィールドに格納された値がすべて一致していればtrueそうでなければfalseを返却する関数*/
+bool	check_t_data_structure(t_data *target, t_data *test)
+{
+	unsigned int	i;
+
+	if (target->height != test->height)
+		return (false);
+	if (target->width != test->width)
+		return (false);
+	if (target->player.x != test->player.x)
+		return (false);
+	if (target->player.y != test->player.y)
+		return (false);
+	if (target->player.dir != test->player.dir)
+		return (false);
+	if (target->f_color != test->f_color)
+		return (false);
+	if (target->c_color != test->c_color)
+		return (false);
+	if (target->map_enum != NULL && test->map_enum != NULL)
+	{
+		i = 0;
+		while (i < target->height * target->width)
+		{
+			if (target->map_enum[i] != test->map_enum[i])
+				return (false);
+			i++;
+		}
+	}
+	else if (target->map_enum != test->map_enum)
+		return (false);
+	return (true);
+}
---
+++ test/unit-tests/load/init_test00.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_test00.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 17:15:30 by miyuu             #+#    #+#             */
+/*   Updated: 2025/05/18 13:54:04 by miyuu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+bool	check_t_data_structure(t_data *target, t_data *test);
+
+/*
+NO ./textures/north.xpm
+SO ./textures/south.xpm
+WE ./textures/west.xpm
+EA ./textures/east.xpm
+
+F 220,100,0
+C 225,30,0
+
+111111
+100101
+101001
+1100N1
+111111
+*/
+// 上記のような.cubがよみこまれた場合、t_dataに格納されるべきデータ
+void	init_test00(void)
+{
+	char	*input_string;	// <- 問題
+	t_data	*target;		// <- 答え
+	t_data	*data;			// <- 自作データ
+
+	int	map[] = \
+	{
+		WALL, WALL, WALL, WALL, WALL, WALL, \
+		WALL, EMPTY, EMPTY, WALL, EMPTY, WALL, \
+		WALL, EMPTY, WALL, EMPTY, EMPTY, WALL, \
+		WALL, WALL, EMPTY, EMPTY, EMPTY, WALL, \
+		WALL, WALL, WALL, WALL, WALL, WALL \
+	};
+	target = &(t_data){\
+		NULL, NULL, \
+		5, 6, \
+		NULL, map, (t_pos){4, 3, DIR_NORTH}, \
+		NULL, NULL, NULL, NULL, 0xDC6400, 0xE11E00};
+
+	input_string = "map/test_parse/ok/simple.cub";
+	data = init_cubdata(input_string);
+	data -> mlx = NULL;
+
+	debug_print_data((t_data *)target);
+	// 構造体の各フィールドに格納された値がすべて一致していればtrue、そうでなければfalseを返却する関数
+	if (check_t_data_structure(target, data))
+		printf("OK!\n");
+	else
+		printf("ERROR\n");
+}
+
+int	main(void)
+{
+	init_test00();
+	return (0);
+}
