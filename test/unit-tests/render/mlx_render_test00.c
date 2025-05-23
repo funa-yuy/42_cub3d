5b0f9e543d2c669b087b54fa5461489fe6da58c4
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-05-23 13:58:51 +0900

    [WIP] xpmファイルを読み取る関数の使い方を確認

--- test/unit-tests/render/mlx_render_test00.c
+++ test/unit-tests/render/mlx_render_test00.c
@@ -1,18 +1,10 @@
 #include "cub3d.h"
 #include "mlx_int.h"
 #include <stdint.h>
+#include <unistd.h>
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 600
-
-typedef struct s_mlx_data t_mlx_data;
-struct s_mlx_data{
-	void *mlx_ptr;
-	void *mlx_win;
-	void *mlx_img;
-	uint32_t *mlx_addr;
-};
-
 
 void	put_point(
 	uint32_t *mlx_addr,
@@ -27,13 +19,29 @@ void	put_point(
 }
 
 
-/// 
+uint32_t *get_image(void *mlx_ptr, char *filename)
+{
+	//int size;
+	int width;
+	int height;
+	uint32_t *img;
+
+	//size = 64;
+	img = mlx_xpm_file_to_image(mlx_ptr, filename, &width, &height);
+	debug_dprintf(STDERR_FILENO, "width(%d) height(%d)\n", width, height);
+	return (img);
+}
+
+/// ```bash
+/// make test test/unit-tests/render/mlx_render_test00.c
+/// ```
 int main()
 {
 	void *mlx_ptr;
 	void *mlx_win;
-	void *mlx_img;
+	uint32_t *mlx_img;
 	uint32_t *mlx_addr;
+	uint32_t *mlx_small_img;
 	int bpp;
 	int size_line;
 	int endian;
@@ -50,7 +58,9 @@ int main()
 		mlx_addr[i] = 0x00ff00;
 		i++;
 	}
+	mlx_small_img = get_image(mlx_ptr, "./textures/east.xpm");
 	mlx_put_image_to_window(mlx_ptr, mlx_win, mlx_img, 0, 0);
+	mlx_put_image_to_window(mlx_ptr, mlx_win, mlx_small_img, 0, 0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
