30e9203147712188f9f9fa3ae4bab2b494d038ec
funa <orz.home35@gmail.com>
2025-05-25 20:07:00 +0900

    📝 mac用のコメントアウトを元に戻した

--- src/load/free_data.c
+++ src/load/free_data.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 14:30:32 by miyuu             #+#    #+#             */
-/*   Updated: 2025/05/25 14:22:20 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/05/25 20:06:23 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	free_data(t_data *d)
 	if (d->mlx)
 	{
 		/* ---- macの場合、以下をコメントアウト ---- */
-		// mlx_destromy_display(d->mlx);
+		mlx_destromy_display(d->mlx);
 		free(d->mlx);
 	}
 	free(d);
