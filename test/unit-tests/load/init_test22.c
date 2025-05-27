7951fe78c2fad9c67de9b2e92e5bd19c43d12417
funa <orz.home35@gmail.com>
2025-05-28 01:13:30 +0900

    📝 correctのmapを追加&testも追加

--- test/unit-tests/load/init_test22.c
+++ test/unit-tests/load/init_test22.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_test17.c                                      :+:      :+:    :+:   */
+/*   init_test22.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:15:30 by miyuu             #+#    #+#             */
-/*   Updated: 2025/05/28 00:44:55 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/05/28 00:59:28 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,22 @@
 bool	check_t_data_structure(t_data *target, t_data *test);
 
 /*
-map/correct/color_plus.cub
+map/correct/map_05.cub
 
 NO ./textures/north.xpm
 SO ./textures/south.xpm
 WE ./textures/west.xpm
 EA ./textures/east.xpm
-F +220,100,0
+F 220,100,0
 C 225,30,0
-111111
-100101
-101001
-1100N1
-111111
+000000000
+011111110
+010000010
+010000E10
+011111110
+000000000
 */
-int	init_test17(void)
+int	init_test22(void)
 {
 	char	*input_string;	// <- 問題
 	t_data	*target;		// <- 答え
@@ -37,23 +38,24 @@ int	init_test17(void)
 
 	int	map[] = \
 	{
-		WALL, WALL, WALL, WALL, WALL, WALL, \
-		WALL, EMPTY, EMPTY, WALL, EMPTY, WALL, \
-		WALL, EMPTY, WALL, EMPTY, EMPTY, WALL, \
-		WALL, WALL, EMPTY, EMPTY, EMPTY, WALL, \
-		WALL, WALL, WALL, WALL, WALL, WALL \
+		EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, \
+		EMPTY, WALL, WALL, WALL, WALL, WALL, WALL, WALL, EMPTY, \
+		EMPTY, WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL, EMPTY, \
+		EMPTY, WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WALL, EMPTY, \
+		EMPTY, WALL, WALL, WALL, WALL, WALL, WALL, WALL, EMPTY, \
+		EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, \
 	};
 	target = &(t_data){\
 		NULL, NULL, \
 		NULL, NULL, NULL, NULL, \
 		0xDC6400, 0xE11E00, /* f_color, c_color */\
-		5, 6, /* height,width */\
-		(t_pos){3, 4, DIR_NORTH}, map};/*player, map*/
+		6, 9, /* height,width */\
+		(t_pos){3, 6, DIR_EAST}, map};/*player{y, x, dir}, map*/
 	printf("\x1b[32m ================ ↓正解↓ ================ \x1b[39m\n");
 	debug_print_data((t_data *)target);
 
 	printf("\x1b[32m ================ ↓自作↓ ================= \x1b[39m\n");
-	input_string = "map/correct/color_plus.cub";
+	input_string = "map/correct/map_05.cub";
 	data = init_cubdata(input_string);
 	data -> mlx = NULL;
 
@@ -73,6 +75,6 @@ int	main(void)
 {
 	int	status;
 
-	status = init_test17();
+	status = init_test22();
 	return (status);
 }
