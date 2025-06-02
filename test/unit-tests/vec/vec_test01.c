825dbb19452e747b68d4cf7a31ef4d1a40336183
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-06-02 10:33:08 +0900

    [WIP] テストを書き直す

--- test/unit-tests/vec/vec_test01.c
+++ test/unit-tests/vec/vec_test01.c
@@ -1,29 +1,23 @@
 #include "vec.h"
+#include "cub3d.h"
 
-int test00()
+#include <unistd.h>
+
+int test00(
+	t_f32x4 a,
+	t_f32x4 b
+)
 {
-	t_f32x4 a;
-	t_f32x4 b;
+	t_vec_f32x4 a_s;
+	t_vec_f32x4 b_s;
 
-	a = init_f32x4(0, 2, 8, 18);
-	b = init_f32x4(0, 1, 1, 3);
-	
+	a_s = f32x4_to_struct(a);
+	b_s = f32x4_to_struct(b);
+	debug_dprintf(STDERR_FILENO, "%f*x + %f*y = %f\n", a_s.x, a_s.y, a_s.z);
+	debug_dprintf(STDERR_FILENO, "%f*x + %f*y = %f\n", b_s.x, b_s.y, b_s.z);
 	print_f32x4("linear_solver", linear_solver(a, b));
 	return (0);
 }
-
-int test01()
-{
-	t_f32x4 a;
-	t_f32x4 b;
-
-	a = init_f32x4(0, 5, -5, 10);
-	b = init_f32x4(0, 1, 10, 57);
-	
-	print_f32x4("linear_solver", linear_solver(a, b));
-	return (0);
-}
-
 
 
 /// ```
@@ -31,7 +25,12 @@ int test01()
 /// ```
 int main()
 {
-	test00();
-	test01();
-
+	test00(
+		init_f32x4(0, 2, 8, 18),
+		init_f32x4(0, 1, 1, 3)
+	);
+	test00(
+		init_f32x4(0, 5, -5, 10),
+		init_f32x4(0, 1, 10, 57)
+	);
 }
