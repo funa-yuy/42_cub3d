af6f92dfb338fa37aa9843e7f6318a4795472678
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-06-03 14:05:09 +0900

    [WIP] テスト関数の整形

--- test/unit-tests/vec/vec_test00.c
+++ test/unit-tests/vec/vec_test00.c
@@ -1,6 +1,8 @@
 #include "vec.h"
+#include "cub3d.h"
 
 #include <assert.h>
+#include <unistd.h>
 
 
 /// how to access each elements of vector
@@ -12,27 +14,25 @@ int main()
 {
 	t_f32x4 a;
 	t_f32x4 b;
-	t_vec_f32x4 *vc;
-	float arr[4];
+	t_vec_f32x4 vc;
 
 	a = init_f32x4(1.0f, 2.0f, 3.0f, 4.0f);
 	b = init_f32x4(4.0f, 3.0f , 2.0f, 1.0f);
-	set_f32x4_to_arr(arr, a);
-	vc = (t_vec_f32x4 *) arr;
+	vc = f32x4_to_struct(a);
 
 	assert(
-		vc->w == 1.0f && \
-		vc->x == 2.0f && \
-		vc->y == 3.0f && \
-		vc->z == 4.0f
+		vc.w == 1.0f && \
+		vc.x == 2.0f && \
+		vc.y == 3.0f && \
+		vc.z == 4.0f
 	);
-	set_f32x4_to_arr(arr, b);
-	vc = (t_vec_f32x4 *) arr;
+	vc = f32x4_to_struct(b);
 	assert(
-		vc->w == 4.0f && \
-		vc->x == 3.0f && \
-		vc->y == 2.0f && \
-		vc->z == 1.0f
+		vc.w == 4.0f && \
+		vc.x == 3.0f && \
+		vc.y == 2.0f && \
+		vc.z == 1.0f
 	);
+	debug_dprintf(STDERR_FILENO, "Ok!\n");
 	return (0);
 }
--- test/unit-tests/vec/vec_test02.c
+++ test/unit-tests/vec/vec_test02.c
@@ -1,32 +1,61 @@
 #include "vec.h"
-#include  "line_segment.h"
+#include "line_segment.h"
+#include "cub3d.h"
+#include <assert.h>
+#include <unistd.h>
 
+/// 交わるケース
+void test00()
+{
+	t_f32x4 p;
+
+	p = cross_point(
+		// t_line_segment(線分)は２つの位置ベクトルから構成される
+		// 今回は二次元平面しか扱わないため、x成分、y成分のみ有効
+		(t_line_segment) { 
+			.s=init_f32x4(0, 0, 0, 0),
+			.e=init_f32x4(0, 1, 1, 0)
+		},
+		(t_line_segment) {
+			.s=init_f32x4(0, 1, 0, 0),
+			.e=init_f32x4(0, 0, 1, 0)
+		}
+	);
+	print_f32x4("cross point", p);
+	if (eq_f32x4(p, init_f32x4(0, 0.5, 0.5, 0))) // 予想される交点
+		debug_dprintf(STDERR_FILENO, "Ok\n");
+	else
+		debug_dprintf(STDERR_FILENO, "KO\n");
+}
+
+/// 直線では交わるが線分では交わらないケース
+void test01()
+{
+	t_f32x4 p;
+
+	p = cross_point(
+		// t_line_segment(線分)は２つの位置ベクトルから構成される
+		// 今回は二次元平面しか扱わないため、x成分、y成分のみ有効
+		(t_line_segment) { 
+			.s=init_f32x4(0, 0, 0, 0),
+			.e=init_f32x4(0, 1, 0, 0)
+		},
+		(t_line_segment) {
+			.s=init_f32x4(0, 0, 3, 0),
+			.e=init_f32x4(0, 1, 2, 0)
+		}
+	);
+	print_f32x4("cross point", p);
+	if (eq_f32x4(p, init_f32x4(1, 0, 0, 0))) // エラーの場合はwに1が格納される
+		debug_dprintf(STDERR_FILENO, "Ok\n");
+	else
+		debug_dprintf(STDERR_FILENO, "KO\n");
+}
 /// ```
 /// make test test/unit-tests/vec/vec_test02.c
 /// ```
 int main()
 {
-	t_f32x4 a;
-	t_f32x4 b;
-	t_f32x4 c;
-	t_f32x4 d;
-
-	t_f32x4 p;
-
-	a = init_f32x4(0, 0, 0, 0);
-	b = init_f32x4(0, 1, 1, 0);
-	c = init_f32x4(0, 1, 0, 0);
-	d = init_f32x4(0, 0, 1, 0);
-	p = cross_point(
-		(t_line_segment) {
-			.s=a,
-			.e=b
-		},
-		(t_line_segment) {
-			.s=c,
-			.e=d
-		}
-	);
-	print_f32x4("cross point", p);
-	return (0);
+	test00();
+	test01();
 }
