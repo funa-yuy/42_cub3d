89c55a9aec29c90113c935bf25274f35a6ec3e03
mfunakos <orz.home35@gmail.com>
2025-06-28 16:48:04 +0900

    main.cのいらない処理消した

--- src/main.c
+++ src/main.c
@@ -6,26 +6,11 @@
 /*   By: mfunakos <mfunakos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:44:56 by mfunakos          #+#    #+#             */
-/*   Updated: 2025/06/28 16:16:51 by mfunakos         ###   ########.fr       */
+/*   Updated: 2025/06/28 16:47:27 by mfunakos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// void	render_scene(t_data *data)
-// {
-// 	ft_memset(data->mlx_addr, 0, \
-// 			WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(uint32_t));
-// 	render_wall_to_screen(\
-// 		data, \
-// 		(t_axis_xy_frames){\
-// 			.axis_x_frames = init_axis_x_frames(data), \
-// 			.axis_y_frames = init_axis_y_frames(data), \
-// 		},
-// 		init_f32x4(0, data->player.x, data->player.y, \
-// 					data->player.angle));
-// 	mlx_put_image_to_window(data->mlx, data->win, data->mlx_img, 0, 0);
-// }
 
 int	close_window(t_data *data)
 {
@@ -36,18 +21,17 @@ int	close_window(t_data *data)
 
 int	key_press(int keycode, t_data *data)
 {
-	debug_dprintf(STDERR_FILENO, "キー入力: %d\n", keycode);
 	if (keycode == KEY_ESC)
 		close_window(data);
 	// else
 		//todo: 移動のキー操作 handle_key_input(keycode, data);
-	// todo: 移動後の描画: render_scene(data);
+	// todo: 移動後のレンダリング
 	return (0);
 }
 
 void	cub3d_loop(t_data *data)
 {
-	// todo: 初期値の描画: render_scene(data);
+	// todo: 初期値のレンダリング
 	mlx_hook(data->win, 17, 0, close_window, data);
 	mlx_key_hook(data->win, key_press, data);
 	mlx_loop(data->mlx);
