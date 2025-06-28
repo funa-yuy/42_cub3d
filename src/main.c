6332ccc5584a043c02188514853f3caa5f06d12e
funa <orz.home35@gmail.com>
2025-06-28 16:15:21 +0900

    🏥 ESCキーコードの定義をcub3d.hに追加

--- src/main.c
+++ src/main.c
@@ -6,29 +6,26 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:44:56 by mfunakos          #+#    #+#             */
-/*   Updated: 2025/06/28 15:38:28 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/06/28 16:12:22 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-#include "move.h"
-#include "render.h"
-
-void	render_scene(t_data *data)
-{
-	ft_memset(data->mlx_addr, 0, \
-			WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(uint32_t));
-	render_wall_to_screen(\
-		data, \
-		(t_axis_xy_frames){\
-			.axis_x_frames = init_axis_x_frames(data), \
-			.axis_y_frames = init_axis_y_frames(data), \
-		},
-		init_f32x4(0, data->player.x, data->player.y, \
-					data->player.angle));
-	mlx_put_image_to_window(data->mlx, data->win, data->mlx_img, 0, 0);
-}
+// void	render_scene(t_data *data)
+// {
+// 	ft_memset(data->mlx_addr, 0, \
+// 			WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(uint32_t));
+// 	render_wall_to_screen(\
+// 		data, \
+// 		(t_axis_xy_frames){\
+// 			.axis_x_frames = init_axis_x_frames(data), \
+// 			.axis_y_frames = init_axis_y_frames(data), \
+// 		},
+// 		init_f32x4(0, data->player.x, data->player.y, \
+// 					data->player.angle));
+// 	mlx_put_image_to_window(data->mlx, data->win, data->mlx_img, 0, 0);
+// }
 
 int	close_window(t_data *data)
 {
@@ -42,18 +39,8 @@ int	key_press(int keycode, t_data *data)
 	debug_dprintf(STDERR_FILENO, "キー入力: %d\n", keycode);
 	if (keycode == KEY_ESC)
 		close_window(data);
-	else if (keycode == KEY_W)
-		update_position_front(data);
-	else if (keycode == KEY_A)
-		update_position_left(data);
-	else if (keycode == KEY_S)
-		update_position_back(data);
-	else if (keycode == KEY_D)
-		update_position_right(data);
-	else if (keycode == KEY_LEFT)
-		update_angle_left(data);
-	else if (keycode == KEY_RIGHT)
-		update_angle_right(data);
+	else
+		//todo: 移動のキー操作 handle_key_input(keycode, data);
 	// todo: 移動後の描画: render_scene(data);
 	return (0);
 }
